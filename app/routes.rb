fbd1fc9b7b46ecdbef17448ac91b0114727132ac
fvazquezf <fvazquezf@fi.uba.ar>
2022-06-18 13:10:44 -0300

    saco metodos viejos

--- app/routes.rb
+++ app/routes.rb
@@ -17,46 +17,6 @@ class Routes
     bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{message.from.first_name}")
   end
 
-  on_message_pattern %r{/say_hi (?<name>.*)} do |bot, message, args|
-    bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{args['name']}")
-  end
-
-  on_message '/stop' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: "Chau, #{message.from.username}")
-  end
-
-  on_message '/time' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: "La hora es, #{Time.now}")
-  end
-
-  on_message '/tv' do |bot, message|
-    kb = Tv::Series.all.map do |tv_serie|
-      Telegram::Bot::Types::InlineKeyboardButton.new(text: tv_serie.name, callback_data: tv_serie.id.to_s)
-    end
-    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-
-    bot.api.send_message(chat_id: message.chat.id, text: 'Quien se queda con el trono?', reply_markup: markup)
-  end
-
-  on_message '/busqueda_centro' do |bot, message|
-    kb = [
-      Telegram::Bot::Types::KeyboardButton.new(text: 'Compartime tu ubicacion', request_location: true)
-    ]
-    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-    bot.api.send_message(chat_id: message.chat.id, text: 'Busqueda por ubicacion', reply_markup: markup)
-  end
-
-  on_location_response do |bot, message|
-    response = "Ubicacion es Lat:#{message.location.latitude} - Long:#{message.location.longitude}"
-    puts response
-    bot.api.send_message(chat_id: message.chat.id, text: response)
-  end
-
-  on_response_to 'Quien se queda con el trono?' do |bot, message|
-    response = Tv::Series.handle_response message.data
-    bot.api.send_message(chat_id: message.message.chat.id, text: response)
-  end
-
   on_message '/version' do |bot, message|
     bot.api.send_message(chat_id: message.chat.id, text: Version.current)
   end
