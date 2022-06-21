2fde1c87438e4cfef1411aa7619d08b068616cd2
lautarofritz <lautarofritz2@gmail.com>
2022-06-20 22:45:50 -0300

    Agrego clase para formatear mensajes

---
+++ app/parser_mensajes.rb
@@ -0,0 +1,35 @@
+class ParserDeMensajes
+  def registro_exitoso(nombre)
+    "Bienvenido #{nombre}!, te registraste exitosamente."
+  end
+
+  def registro_no_exitoso_datos_invalidos
+    'Datos invalidos, ingrese un telefono de 10 digitos, un nombre valido y una direccion. /registrar Francisco, 1144449999, paseo colon 850'
+  end
+
+  def registro_no_exitoso_ya_registrado
+    'Ya estás registrado'
+  end
+
+  def mostrar_menus(menus)
+    mensaje = []
+    menus.each { |menu| mensaje.push("#{menu.id}-#{menu.descripcion} ($#{menu.precio})") }
+    mensaje.join(', ')
+  end
+
+  def pedido_exitoso(id_pedido)
+    "Su pedido ha sido registrado: N° #{id_pedido}."
+  end
+
+  def pedido_invalido
+    'No se pudo realizar el pedido. Asegurarse de estar registrado o solicitar un menu valido.'
+  end
+
+  def consulta_estado_exitosa(id_pedido, estado)
+    "Pedido #{id_pedido}: #{estado}"
+  end
+
+  def consulta_estado_no_exitosa
+    'No se pudo consultar el pedido. Asegurese de que el pedido exista.'
+  end
+end
--- app/routes.rb
+++ app/routes.rb
@@ -2,6 +2,7 @@ require "#{File.dirname(__FILE__)}/../lib/routing"
 require "#{File.dirname(__FILE__)}/../lib/version"
 require "#{File.dirname(__FILE__)}/tv/series"
 require_relative 'api_bobe.rb'
+require_relative 'parser_mensajes.rb'
 require 'byebug'
 require_relative 'errors/usuario_invalido.rb'
 require_relative 'errors/usuario_ya_registrado.rb'
@@ -12,6 +13,7 @@ class Routes
   api_bobe = APIBobe.new
   # TODO: Ubicar el logger dentro del bot
   bot_logger = SemanticLogger['BotClient']
+  parser = ParserDeMensajes.new
 
   on_message '/start' do |bot, message|
     bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{message.from.first_name}")
@@ -27,42 +29,40 @@ class Routes
 
   on_message_pattern %r{/registrar (?<nombre>.*), (?<telefono>.*), (?<direccion>.*)} do |bot, message, args|
     usuario = api_bobe.registro_usuario(args['nombre'], args['telefono'], args['direccion'], message.from.id.to_s)
-    bot.api.send_message(chat_id: message.chat.id, text: "Bienvenido #{usuario.nombre}!, te registraste exitosamente.")
+    bot.api.send_message(chat_id: message.chat.id, text: parser.registro_exitoso(usuario.nombre))
     bot_logger.info "Registro de usuario exitoso: #{args} "
   rescue UsuarioInvalido
     bot_logger.info "Error al registrar usuario: #{args}"
-    bot.api.send_message(chat_id: message.chat.id, text: 'Datos invalidos, ingrese un telefono de 10 digitos, un nombre valido y una direccion. /registrar Francisco, 1144449999, paseo colon 850')
+    bot.api.send_message(chat_id: message.chat.id, text: parser.registro_no_exitoso_datos_invalidos)
   rescue UsuarioYaRegistrado
     bot_logger.info "El usuario ya estaba registrado: #{args}"
-    bot.api.send_message(chat_id: message.chat.id, text: 'Ya estás registrado')
+    bot.api.send_message(chat_id: message.chat.id, text: parser.registro_no_exitoso_ya_registrado)
   end
 
   on_message_pattern %r{/registrar (?<nombre>.*), (?<direccion>.*)} do |bot, message, args|
     bot_logger.info "Error de parametros en registrar: #{args}"
-    bot.api.send_message(chat_id: message.chat.id, text: 'Datos invalidos, ingrese un telefono de 10 digitos, un nombre valido y una direccion. /registrar Francisco, 1144449999, paseo colon 850')
+    bot.api.send_message(chat_id: message.chat.id, text: parser.registro_no_exitoso_datos_invalidos)
   end
 
   on_message '/menus' do |bot, message|
     menus = api_bobe.pedir_menus
-    mensaje = []
-    menus.each { |menu| mensaje.push("#{menu.id}-#{menu.descripcion} ($#{menu.precio})") }
-    bot.api.send_message(chat_id: message.chat.id, text: mensaje.join(', '))
+    bot.api.send_message(chat_id: message.chat.id, text: parser.mostrar_menus(menus))
   end
 
   on_message_pattern %r{/pedir (?<menu>.*)} do |bot, message, args|
     pedido = api_bobe.hacer_pedido(message.from.id.to_i, args['menu'].to_i)
-    bot.api.send_message(chat_id: message.chat.id, text: "Su pedido ha sido registrado: N° #{pedido.id_pedido}.")
+    bot.api.send_message(chat_id: message.chat.id, text: parser.pedido_exitoso(pedido.id_pedido))
   rescue PedidoInvalido
     bot_logger.info "Error al realizar pedido: #{args}"
-    bot.api.send_message(chat_id: message.chat.id, text: 'No se pudo realizar el pedido. Asegurarse de estar registrado o solicitar un menu valido.')
+    bot.api.send_message(chat_id: message.chat.id, text: parser.pedido_invalido)
   end
 
   on_message_pattern %r{/consultar (?<numeroPedido>.*)} do |bot, message, args|
     pedido = api_bobe.consultar_pedido(message.from.id.to_i, args['numeroPedido'].to_i)
-    bot.api.send_message(chat_id: message.chat.id, text: "Pedido #{pedido.id_pedido}: #{pedido.estado}")
+    bot.api.send_message(chat_id: message.chat.id, text: parser.consulta_estado_exitosa(pedido.id_pedido, pedido.estado))
   rescue PedidoInvalido
     bot_logger.info "Pedido invalido: #{args}"
-    bot.api.send_message(chat_id: message.chat.id, text: 'No se pudo consultar el pedido. Asegurese de que el pedido exista.')
+    bot.api.send_message(chat_id: message.chat.id, text: parser.consulta_estado_no_exitosa)
   end
 
   default do |bot, message|
